b3ac24f5f0146216e6b6ba2b6cd1e5863a845797
truptibankar <truptibankar12@gmail.com>
2020-08-03 22:16:52 +0530

    this is my first push repositary into git

---
+++ src/FishInheritance/Fish.java
@@ -0,0 +1,10 @@
+package FishInheritance;
+
+public class Fish {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/FishInheritance/FishInheritanceTest.java
@@ -0,0 +1,10 @@
+package FishInheritance;
+
+public class FishInheritanceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
